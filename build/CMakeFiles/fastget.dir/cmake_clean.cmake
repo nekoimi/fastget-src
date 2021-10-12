c3a9519db28833f4419b126b9d94c847c1972cfc
nekoimi <nekoimime@gmail.com>
2021-10-12 16:46:52 +0800

    create

---
+++ build/CMakeFiles/fastget.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fastget.dir/main.c.obj"
+  "fastget.exe"
+  "fastget.exe.manifest"
+  "fastget.pdb"
+  "libfastget.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fastget.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
